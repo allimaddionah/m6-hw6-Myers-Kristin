f57b6a33eec9500a0381687449923ae58936cd7d
Kristin Myers <kristinmyers@outlook.com>
2019-10-06 15:06:56 -0400

    Add JS item iii

--- main.js
+++ main.js
@@ -30,3 +30,26 @@ var startButton = document.querySelector('.heading button');
 var placeholder1 = document.getElementById('Placeholder1');
 var placeholder2 = document.getElementById('Placeholder2');
 var placeholder3 = document.getElementById('Placeholder3');
+
+
+//This is e.iii with e.ii.
+
+
+function showMessage1() {
+  placeholder1.appendChild(message1);
+  startButton.classList.remove('hide');
+}
+
+function showMessage2() {
+  placeholder2.appendChild(message2);
+  startButton.classList.remove('hide');
+}
+
+function showMessage3() {
+  placeholder3.appendChild(message3);
+  startButton.classList.remove('hide');
+}
+
+box1.addEventListener('click', showMessage1);
+box2.addEventListener('click', showMessage2);
+box3.addEventListener('click', showMessage3);
